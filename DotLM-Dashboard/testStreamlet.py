f6845d80914dba4cb429e4dba5c9e88ad40758c7
JoeMJr <josephmiddletonjr@outlook.com>
2026-02-23 22:20:09 -0500

    new file:   DotLM-Dashboard/testStreamlet.py 	new file:   DotLM-Dashboard/textthing.txt

---
+++ DotLM-Dashboard/testStreamlet.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("DotLM Web FrontEnd Test")
+st.markdown(
+    """ 
+    This is a steamlet test, I am just seeing if this can work well for a web frontend
+
+    **Heres :rainbow[Rainbow Text]**
+    """
+)
+
+if st.button("balloon test"):
+    st.balloons()
